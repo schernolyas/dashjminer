2508a0c5123d1b500a775502d746854ab3d06e31
schernolyas <sergey.chernolyas@gmail.com>
2015-09-07 16:19:12 +0300

    corrections

--- client/src/test/java/ru/schernolyas/stratum/client/utils/DifficultyUtilTest.java
+++ client/src/test/java/ru/schernolyas/stratum/client/utils/DifficultyUtilTest.java
@@ -6,6 +6,8 @@
 package ru.schernolyas.stratum.client.utils;
 
 import java.math.BigDecimal;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.apache.commons.codec.binary.Hex;
 import org.junit.After;
 import org.junit.AfterClass;
@@ -20,27 +22,25 @@ import ru.schernolyas.stratum.client.method.SetDifficulty;
  * @author schernolyas
  */
 public class DifficultyUtilTest {
-    
+
     public DifficultyUtilTest() {
     }
-    
+
     @BeforeClass
     public static void setUpClass() {
     }
-    
+
     @AfterClass
     public static void tearDownClass() {
     }
-    
+
     @Before
     public void setUp() {
     }
-    
+
     @After
     public void tearDown() {
     }
-
-   
 
     /**
      * Test of calculateTarget method, of class DifficultyUtil.
@@ -48,17 +48,23 @@ public class DifficultyUtilTest {
     @Test
     public void testCalculateTarget_BigDecimal() throws Exception {
         System.out.println("calculateTarget");
-        BigDecimal decimalDifficulty = new BigDecimal("0.03125"); 
+        BigDecimal decimalDifficulty = new BigDecimal("0.03125");
+
         byte[] result = DifficultyUtil.calculateTarget(decimalDifficulty);
         //assertArrayEquals(expResult, result);
         System.out.println(Hex.encodeHexString(result));
-        
-        decimalDifficulty = new BigDecimal("0.12");        
+
+        decimalDifficulty = new BigDecimal("0.12");
+        String h = Float.toHexString(decimalDifficulty.floatValue());
+        Pattern p = Pattern.compile("0x1\\.(.*)p(.*)");
+        Matcher matcher = p.matcher(h);
+        if (matcher.find()) {
+            System.out.println("group1: " + matcher.group(1) + ";" + matcher.group(2));
+        }
+        System.out.println("!" + Float.toHexString(decimalDifficulty.floatValue()));
         result = DifficultyUtil.calculateTarget(decimalDifficulty);
         //assertArrayEquals(expResult, result);
         System.out.println(Hex.encodeHexString(result));
     }
 
-   
-    
 }
